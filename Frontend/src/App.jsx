0a8dc9e58d6a5b7a680362c277ed635b0efa508a
Bhavika Maski <maskibhavika@gmail.com>
2026-09-22 20:36:39 +0530

    job page, jobs routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+ import { Routes, Route } from "react-router-dom";
+import Jobs from "./pages/JobsPage";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/jobs" element={<Jobs />} />
       </Routes>
     </>
   );
